3595f992f0f079605801d387031c73b4288d0991
lonewanderer27 <adrianejames27@gmail.com>
2022-08-27 01:17:12 +0800

    add Bootstrap import in main.jsx

--- react-bootstrap/src/main.jsx
+++ react-bootstrap/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
