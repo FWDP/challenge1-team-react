8650048326b1f1cae6a58c73c417c230127ad68d
lonewanderer27 <adrianejames27@gmail.com>
2022-08-27 01:16:36 +0800

    add team data

---
+++ react-bootstrap/src/teamData.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        key: 1,
+        username: "lonewanderer27#8068",
+        profilePic: "lonewanderer27.png",
+        url: "",
+    },
+    {
+        key: 2,
+        username: "Jaezer#0100",
+        profilePic: "Jaezer.png",
+        url: "",
+    },
+    {
+        key: 3,
+        username: "coscov#5692",
+        profilePic: "coscov.png",
+        url: "",
+    },
+    {
+        key: 4,
+        username: "huron#3261",
+        profilePic: "huron.png",
+        url: "",
+    },
+    {
+        key: 5,
+        username: "jraylim123#6596",
+        profilePic: "jraylim123.png",
+        url: "",
+    },
+    {
+        key: 6,
+        username: "K A M O T E#9336",
+        profilePic: "KAMOTE.png",
+        url: "",
+    },
+    {
+        key: 7,
+        username: "Licht#4662",
+        profilePic: "Licht.png",
+        url: "",
+    },
+    {
+        key: 8,
+        username: "Mr. H#0196",
+        profilePic: "Mr.H.png",
+        url: "",
+    },
+    {
+        key: 9,
+        username: "rainrain#5258",
+        profilePic: "rainrain.png",
+        url: "",
+    },
+    {
+        key: 10,
+        username: "codewithpau#8824",
+        profilePic: "codewithpau.png",
+        url: "",
+    },
+    {
+        key: 11,
+        username: "Kevin Mensah#5021",
+        profilePic: "KevinMensah.png",
+        url: "",
+    },
+    {
+        key: 12,
+        username: "Mench#0636",
+        profilePic: "Mench.png",
+        url: "",
+    }
+]
