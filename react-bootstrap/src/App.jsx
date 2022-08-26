f83cb9a97d43ddf17478e6823d7d695a6304ae94
lonewanderer27 <adrianejames27@gmail.com>
2022-08-27 01:17:42 +0800

    modify App.css and App.jsx

--- react-bootstrap/src/App.jsx
+++ react-bootstrap/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import teamData from './teamData'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(teamData)
+  const teamImages = teamData.map(member => {
+    return <img 
+              key={member.key} 
+              src={`/images/Team/${member.profilePic}`} 
+              style={{height: "100px", width: "100px"}} 
+    />
+  })
+  console.log(teamImages)
   return (
     <div className="App">
-      
+      <Navbar />
+      {teamImages}
     </div>
   )
 }
